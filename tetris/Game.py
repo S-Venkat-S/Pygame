d81844b7a2305486c6c4d9f959b0f65f0d4631ac
venkatesh18596@gmail.com <venkatesh18596@gmail.com>
2019-08-11 17:04:15 +0530

    Tetri rotation and removing the solved line pending.

--- tetris/Game.py
+++ tetris/Game.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 from common.Utils import Utils
 from Constants import Constants
 from common.Constants import Constants as Cons
@@ -8,62 +9,126 @@ from common.Constants import Constants as Cons
 class Game:
 
     tetri_i = [(-1, 3), (-1, 4), (-1, 5), (-1, 6)]  # (y, x) representation
-    speed = 0.10
+    tetri_o = [(-1, 4), (-1, 5), (-2, 4), (-2, 5)]  # (y, x) representation
+    tetri_t = [(-1, 4), (-2, 3), (-2, 4), (-2, 5)]  # (y, x) representation
+    tetri_j = [(-1, 5), (-2, 3), (-2, 4), (-2, 5)]  # (y, x) representation
+    tetri_l = [(-1, 3), (-2, 3), (-2, 4), (-2, 5)]  # (y, x) representation
+    tetri_s = [(-1, 3), (-1, 4), (-2, 4), (-2, 5)]  # (y, x) representation
+    tetri_z = [(-2, 3), (-2, 4), (-1, 4), (-1, 5)]  # (y, x) representation
+
+    tetris_list = [tetri_i, tetri_o, tetri_t, tetri_j,
+                   tetri_l, tetri_s, tetri_z]
+    speed = .10
     old_time = 0
 
     game_data = {
         "game_grid": [],
-        "next_tetri": {},
-        "current_tetri": list(tetri_i)
+        "next_tetri": None,
+        "current_tetri": None
     }
 
     def check_cell(self, y, x):
+        if y < 0:
+            return False
         if self.game_data["game_grid"][y][x]:
             return True
         else:
             return False
 
-    def move_left(self):
-        for i in range(len(self.game_data["current_tetri"])):
-            cur_vec = self.game_data["current_tetri"][i]
-            if (cur_vec[1] - 1) > -1 and not self.check_cell(cur_vec[0], cur_vec[1] - 1):
-                self.game_data["game_grid"][cur_vec[0]][cur_vec[1]] = 0
-                cur_vec = (cur_vec[0], cur_vec[1] - 1)
-                self.game_data["current_tetri"][i] = cur_vec
-                self.game_data["game_grid"][cur_vec[0]][cur_vec[1]] = 1
-            else:
-                return
-        return
+    def get_next_tetri(self):
+        return random.choice(self.tetris_list)
 
-    def move_right(self):
-        for i in range(len(self.game_data["current_tetri"])-1, -1, -1):
-            cur_vec = self.game_data["current_tetri"][i]
-            if (cur_vec[1] + 1) < self.grid_w and not self.check_cell(cur_vec[0], cur_vec[1] + 1):
-                self.game_data["game_grid"][cur_vec[0]][cur_vec[1]] = 0
-                cur_vec = (cur_vec[0], cur_vec[1] + 1)
-                self.game_data["current_tetri"][i] = cur_vec
-                self.game_data["game_grid"][cur_vec[0]][cur_vec[1]] = 1
-            else:
-                return
-        pass
+    def fill_cells(self, y, x):
+        if y < 0:
+            return True
+        if self.game_data["game_grid"][y][x] == 0:
+            self.game_data["game_grid"][y][x] = 1
+            return True
+        else:
+            return False
 
-    def move_down(self):
-        no_fill = []
-        fill = []
-        for i in range(len(self.game_data["current_tetri"])):
-            cur_vec = self.game_data["current_tetri"][i]
-            if (cur_vec[0] + 1) < self.grid_h and not self.check_cell(cur_vec[0] + 1, cur_vec[1]):
-                no_fill.append(cur_vec)
-                cur_vec = (cur_vec[0] + 1, cur_vec[1])
-                fill.append(cur_vec)
+    def erase_cells(self, y, x):
+        if y < 0:
+            return True
+        if self.game_data["game_grid"][y][x] == 1:
+            self.game_data["game_grid"][y][x] = 0
+            return True
+        else:
+            return False
+
+    def update_current_tetri(self, cells):
+        self.game_data["current_tetri"] = cells
+        return True
+
+    def clear_and_fill(self, no_fill, fill):
+        for j in no_fill:
+            self.erase_cells(j[0], j[1])
+        for i in fill:
+            self.fill_cells(i[0], i[1])
+        return True
+
+    def validate_moves(self, no_fill, fill):
+        for i in fill:
+            if i in no_fill or not self.check_cell(i[0], i[1]):
+                pass
             else:
                 return False
-        for i in range(len(fill)):
-            self.game_data["current_tetri"][i] = fill[i]
-            self.game_data["game_grid"][fill[i][0]][fill[i][1]] = 1
-            if no_fill[i][0] >= 0:
-                self.game_data["game_grid"][no_fill[i][0]][no_fill[i][1]] = 0
         return True
+
+    def move(self, no_fill, fill):
+        # Validate moves
+        if not self.validate_moves(no_fill, fill):
+            return False
+        # Filling and non-filling cells
+        if not self.clear_and_fill(no_fill, fill):
+            return False
+        # Updating the current tetri with the cells to be filled
+        return self.update_current_tetri(fill)
+
+    def move_left(self):
+        no_fill = list(self.game_data["current_tetri"])
+        fill = []
+
+        # Generating list of possible moves
+        for i in no_fill:
+            y = i[0]
+            x = i[1]
+            if x - 1 >= 0:
+                x = x - 1
+                fill.append((y, x))
+            else:
+                return False
+        return self.move(no_fill, fill)
+
+    def move_right(self):
+        no_fill = list(self.game_data["current_tetri"])
+        fill = []
+
+        # Generating list of possible moves
+        for i in no_fill:
+            y = i[0]
+            x = i[1]
+            if x + 1 < Constants.grid_width:
+                x = x + 1
+                fill.append((y, x))
+            else:
+                return False
+        return self.move(no_fill, fill)
+
+    def move_down(self):
+        no_fill = list(self.game_data["current_tetri"])
+        fill = []
+
+        # Generating list of possible moves
+        for i in no_fill:
+            y = i[0]
+            x = i[1]
+            if y + 1 < Constants.grid_height:
+                y = y + 1
+                fill.append((y, x))
+            else:
+                return False
+        return self.move(no_fill, fill)
 
     def key_event(self, event):
         if event.scancode == Cons.key_code_up:
@@ -82,11 +147,13 @@ class Game:
             current_row = []
             for width in range(0, self.grid_w, 1):
                 res = 0
-                if (height, width) in self.game_data["current_tetri"]:
-                    res = 1
+                # if (height, width) in self.game_data["current_tetri"]:
+                #     res = 1
                 current_row.append(res)
             self.game_data["game_grid"].append(current_row)
         # self.game_data["game_grid"][3][5] = 1
+        self.game_data["current_tetri"] = self.get_next_tetri()
+        self.game_data["next_tetri"] = self.get_next_tetri()
         return True
 
     def __init__(self, surface, mock=False):
@@ -103,11 +170,12 @@ class Game:
         return
 
     def update_state(self):
-        for height in range(0, self.grid_h, 1):
-            for width in range(0, self.grid_w, 1):
-                if (height, width) in self.game_data["current_tetri"]:
-                    self.game_data["game_grid"][height][width] = 1
-        return True
+        c_time = time.time()
+        if (c_time - self.old_time) > self.speed:
+            self.old_time = c_time
+            if not self.move_down():
+                self.game_data["current_tetri"] = self.game_data["next_tetri"]
+                self.game_data["next_tetri"] = self.get_next_tetri()
 
     def update_ui_state(self):
         for i in range(0, self.grid_h):
@@ -119,12 +187,7 @@ class Game:
                     self.surface.blit(self.no_fill, (j * Constants.tile_size, i * Constants.tile_size))
 
     def update(self):
-        # self.update_state()
-        c_time = time.time()
-        if (c_time - self.old_time) > self.speed:
-            self.old_time = c_time
-            if not self.move_down():
-                self.game_data["current_tetri"] = list(self.tetri_i)
+        self.update_state()
         self.update_ui_state()
         # self.surface.blit(self.background, (0, 0))
 
